20b5f44c346f77bf9f6f8b9f13767cc25694adc9
Maksim Rudek <maksim.rudek@gmail.com>
2023-04-26 17:37:53 +0300

    Initial commit

---
+++ Assets/AudioVisualizer.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class AudioVisualizer : MonoBehaviour
+{
+    private AudioSource source;
+    public float size = 5;
+    public float power = 3;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        float[] samples = new float[735];
+        source.clip.GetData(samples, source.timeSamples);
+
+        float sum = 0;
+        foreach (var sample in samples)
+        {
+            sum += Mathf.Abs(sample);
+        }
+        float average = sum / 735;
+        print(average);
+
+
+        transform.localScale = Vector3.one * (1 + Mathf.Pow(average, power) * size);
+
+        transform.Rotate(0,average,0);
+    }
+}
